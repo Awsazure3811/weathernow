7a0ee465eda4daa9a6fc7b0aa6a7be227dc9ee1e
sandeeprobustsys <sandeep.dokala@ext.robustsys-gmbh.de>
2023-05-06 12:05:52 +0530

    Initial commit with searchbox input

---
+++ src/app/pages/homepage/homepage.js
@@ -0,0 +1,10 @@
+import "./homepage.css";
+
+export default function HomePage() {
+
+    return(
+        <div className="container">
+            <input type="text" placeholder="Search city" className="search-box" />
+        </div>
+    )
+}
